be58841377855bda49bd9145fe7af42acc430176
HannahGlendell <hannah.glendell@worc.ox.ac.uk>
2024-10-30 10:36:40 +0000

    commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,13 +10,13 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- exp(6.8941709) # from model 1, written as the intercept. need to do exp because output is in log form
   
-r <- ??? #
+r <- 0.0100086 # the gradient from model 1, written in the summary of model as t
   
-K <- ??? #
+K <- 6.000e+10 # the intercept from model 2
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
